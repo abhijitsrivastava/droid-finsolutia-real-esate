ec44a7c8326a3405c0c01dc01424d9038cad3e67
qainfotech <qainfotech@HPDA0015.(none)>
2014-01-28 17:14:09 +0530

    property search item

--- Finsoltia/src/com/finsoltia/AppLaunchActivity.java
+++ Finsoltia/src/com/finsoltia/AppLaunchActivity.java
@@ -9,7 +9,7 @@ import android.os.Handler;
 public class AppLaunchActivity extends Activity {
  
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 1000;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
