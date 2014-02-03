d8d2c24e52672b5aec9adde670c304f1ae6233bf
unknown <trilokinathyadav@HPDA0050.qait.com>
2014-02-03 12:51:33 +0530

    SearchFilterFragment list implemented

--- Finsoltia/src/com/finsolutia/fragment/PropertySearchFilterFragment.java
+++ Finsoltia/src/com/finsolutia/fragment/PropertySearchFilterFragment.java
@@ -1,27 +1,26 @@
 package com.finsolutia.fragment;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
-import android.content.res.Configuration;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
+import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.GridView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.finsoltia.R;
-import com.finsolutia.adapter.PropertySearchAdapter;
-import com.finsolutia.model.ListItem;
 
-public class PropertySearchFilterFragment extends BaseFragment {
+public class PropertySearchFilterFragment extends BaseFragment implements OnClickListener{
 
-View propertySearchFragment;
-List<ListItem> propertyList;
+View propertySearchFilterFragment;
+Button buttonAny,buttonNearMe;
+ListView listViewFilterContainer;
 
 @Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -35,11 +34,11 @@ List<ListItem> propertyList;
     @Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-    	propertySearchFragment = inflater.inflate(R.layout.property_search_filter_fragment, container, false);
-	
+    	propertySearchFilterFragment = inflater.inflate(R.layout.property_search_filter_fragment, container, false);
+	    setUi();
 		
 
-		return propertySearchFragment;
+		return propertySearchFilterFragment;
 	}
 
 	@Override
@@ -47,4 +46,36 @@ List<ListItem> propertyList;
 			
 			super.onAttach(activity);
 		}
+
+	private void setUi() {
+		
+		final String[] MOBILE_OS = new String[] { "Android", "iOS",
+				"WindowsMobile", "Blackberry" };
+		
+		buttonAny = (Button) propertySearchFilterFragment
+				.findViewById(R.id.buttonAny);
+		buttonAny.setOnClickListener(this);
+		buttonNearMe = (Button) propertySearchFilterFragment
+				.findViewById(R.id.buttonNearMe);
+		buttonNearMe.setOnClickListener(this);
+		listViewFilterContainer = (ListView) propertySearchFilterFragment
+				.findViewById(R.id.listViewFilterContainer);
+		listViewFilterContainer.setAdapter(new ArrayAdapter<String>(
+				homeActivity, android.R.layout.simple_list_item_1, MOBILE_OS));
+		listViewFilterContainer
+				.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1,
+							int arg2, long arg3) {
+						homeActivity.popFragment();
+
+					}
+				});
+	}
+	@Override
+	public void onClick(View v) {
+		homeActivity.popFragment();
+		
+	}
 }
